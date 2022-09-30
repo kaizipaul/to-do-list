56c73d467451472b2764edddbefb63015098c56b
kaizipaul <kaizipaul@gmail.com>
2022-09-30 13:48:15 +0300

    Built and styled the project and added some JavaScript functionality

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# webpack-setup
-This is a repo for setting up Webpack, a JavaScript module bundler.
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# To-do List
+
+> This is a project built with Webpack, to implement a basic to-do list.
+
+
+## Built With
+
+- HTML, CSS and JavaScript
+- Webpack
+
+## Live Demo (if available)
+
+[Live Demo Link](https://livedemo.com)
+
+## Authors
+
+👤 **Paul Kaizirege Vedasto**
+
+- GitHub: [@kaizipaul](https://github.com/kaizipaul)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Microverse Inc
+- Web Dev Simplified
+
+## 📝 License
+
+This project is [MIT](./LICENSE) licensed.
--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-import _ from 'lodash';
+/* eslint-disable linebreak-style */
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-    
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+const list = [
+  {
+    description: 'Do some laundry',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Send email to Joffrey',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Do some house chores',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Buy groceries',
+    completed: false,
+    index: 4,
+  },
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+  const listContainer = document.querySelector('.todolist');
+  list.forEach((list) => {
+    const listHtml = `<li class="list-item" id="${list.index}"> <div><i class="fa-regular fa-square-full"></i> <span class="task">${list.description}</span></div><i class="fa-solid fa-ellipsis-vertical"></i></li>`;
+    listContainer.insertAdjacentHTML('beforehand', listHtml);
+  });
+});
