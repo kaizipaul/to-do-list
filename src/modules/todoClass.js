747523a94b1f55e800c4ab3b32933289f5d4712b
Paul Kaizirege <kaizipaul@gmail.com>
2024-06-03 17:51:36 +0300

    add createdAt and updatedAt to task object

--- src/modules/todoClass.js
+++ src/modules/todoClass.js
@@ -1,12 +1,15 @@
+/* eslint-disable max-len */
 export const fromStorage = JSON.parse(localStorage.getItem('listStorage')) || [];
 export const input = document.querySelector('.text-field');
 export const listContainer = document.querySelector('.todolist');
 
 export default class ToDoList {
-  constructor(id, description, complete) {
+  constructor(id, description, complete, createdAt, updatedAt) {
     this.id = id;
     this.description = description;
     this.complete = complete;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
   }
 
   static render = () => {
@@ -14,7 +17,7 @@ export default class ToDoList {
       const taskCard = document.createElement('li');
       taskCard.id = task.id;
       taskCard.classList.add('list-item');
-      taskCard.innerHTML = `<div class="task-main"><input type=checkbox class="check-box"><p class="task-description">${task.description}</p></div><i class="fa-solid fa-trash delete-btn"></i>`;
+      taskCard.innerHTML = `<div class="task-main"><div class="task-column flex"><input type=checkbox class="check-box"><input class="edit-input" type="text" placeholder="Edit Task✒️..." required><p class="task-description">${task.description}</p><p class="created-at">created: ${task.createdAt}</p><p class="created-at">updated: ${task.updatedAt ? task.updatedAt : 'never'}</p></div></div><div class="edit-delete flex"><i class="fa-solid fa-file-pen edit-btn"></i><i class="fa-solid fa-trash delete-btn"></i></div>`;
       listContainer.appendChild(taskCard);
     });
 
@@ -41,10 +44,26 @@ export default class ToDoList {
         ToDoList.delete(id);
       });
     });
+
+    const editBtn = document.querySelectorAll('.edit-btn');
+    const editInput = document.querySelectorAll('.edit-input');
+
+    editBtn.forEach((edit, id) => {
+      edit.addEventListener('click', () => {
+        ToDoList.edit(id);
+      });
+    });
+
+    editInput.forEach((input, id) => {
+      input.addEventListener('blur', () => {
+        ToDoList.saveEdit(id, input.value);
+      });
+    });
   }
 
   static new = () => {
-    const newToDo = new ToDoList(Date.now(), input.value, false);
+    const date = new Date().toString().slice(4, 21);
+    const newToDo = new ToDoList(Date.now(), input.value, false, date, null);
 
     if (input.value !== '') {
       fromStorage.push(newToDo);
@@ -68,7 +87,19 @@ export default class ToDoList {
     localStorage.setItem('listStorage', JSON.stringify(fromStorage));
   }
 
-  static edit = () => {
-    // future update
+  static edit = (index) => {
+    const taskDescription = document.querySelectorAll('.task-description')[index];
+    const editInput = document.querySelectorAll('.edit-input')[index];
+    taskDescription.style.display = 'none';
+    editInput.style.display = 'block';
+    editInput.focus();
+  }
+
+  static saveEdit = (index, newDescription) => {
+    fromStorage[index].description = newDescription;
+    fromStorage[index].updatedAt = new Date().toString().slice(4, 21);
+    localStorage.setItem('listStorage', JSON.stringify(fromStorage));
+    listContainer.innerHTML = '';
+    ToDoList.render();
   }
 }
